4c573020a04e444f6293c3837feb7aebbff01593
Antti Mattila <me@rarelyneeded.com>
2022-02-02 21:54:52 +0200

    Add logging

--- backend/src/core/app.ts
+++ backend/src/core/app.ts
@@ -3,7 +3,7 @@ import { fastifyPostgres } from 'fastify-postgres'
 import { fastifySwagger } from 'fastify-swagger'
 
 import v1 from '../api'
-import { getConfig } from './config'
+import { getConfig, isDevEnvironment } from './config'
 
 const host = getConfig('HOST', 'localhost')
 const port = getConfig('PORT', '4000')
@@ -15,29 +15,41 @@ const database = getConfig('PG_DATABASE')
 const connectionString = `postgres://${user}:${password}@${dbHost}:${dbPort}/${database}`
 
 const initApp = async (): Promise<FastifyInstance> => {
-  const app = fastify()
+  const app = fastify({
+    // https://www.fastify.io/docs/latest/Reference/Logging/
+    logger: {
+      prettyPrint: isDevEnvironment
+        ? {
+            translateTime: 'HH:MM:ss Z',
+            ignore: 'pid,hostname',
+          }
+        : false,
+    },
+  })
   await app.register(fastifyPostgres, {
     connectionString,
   })
-  await app.register(fastifySwagger, {
-    routePrefix: '/documentation',
-    exposeRoute: true,
-    swagger: {
-      info: {
-        title: 'Example API',
-        description: 'API documentation',
-        version: '0.0.1',
+  if (isDevEnvironment) {
+    await app.register(fastifySwagger, {
+      routePrefix: '/documentation',
+      exposeRoute: true,
+      swagger: {
+        info: {
+          title: 'Example API',
+          description: 'API documentation',
+          version: '0.0.1',
+        },
+        host: `http://${host}:${port}`,
+        schemes: ['http'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        tags: [
+          { name: 'Healthcheck', description: 'Healthcheck endpoints' },
+          { name: 'User', description: 'User endpoints' },
+        ],
       },
-      host: `http://${host}:${port}`,
-      schemes: ['http'],
-      consumes: ['application/json'],
-      produces: ['application/json'],
-      tags: [
-        { name: 'Healthcheck', description: 'Healthcheck endpoints' },
-        { name: 'User', description: 'User endpoints' },
-      ],
-    },
-  })
+    })
+  }
   await app.register(v1, { prefix: '/api/v1' })
 
   return app
--- backend/src/features/healthCheck/handlers.ts
+++ backend/src/features/healthCheck/handlers.ts
@@ -1,7 +1,17 @@
 import type { RouteHandler } from 'fastify'
+import { PoolClient } from 'pg'
 
 import { HealthCheckRoute } from './schemas'
 
-export const healthCheck: RouteHandler<HealthCheckRoute> = async (_req, res) =>
-  // TODO: SELECT 1 from DB
-  res.send({ ok: true })
+export const healthCheck: RouteHandler<HealthCheckRoute> = async (req, res) => {
+  const client = await req.server.pg.connect()
+
+  try {
+    const result = await client.query<{ health: number }>('SELECT 1 AS health')
+    return res.send({ ok: result.rows[0]?.health === 1 })
+  } catch (_e) {
+    return res.send({ ok: false })
+  } finally {
+    client.release()
+  }
+}
