645cc29e3094eddb77dc5b22c4458391ae0948c7
Antti Mattila <antti.mattila@futurice.com>
2022-02-03 10:33:13 +0200

    Add React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
 const App = () => {
   return (
     <div className="flex justify-center">
-      <h1 className="font-bold text-2xl text-blue-900">
+      <h1 className="text-2xl font-bold text-blue-900">
         React and Tailwind with Vitejs!
       </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 };
