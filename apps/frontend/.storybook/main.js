864f9a8a2a26624da12e7a2122d4c90caa780ccc
Antti Mattila <me@rarelyneeded.com>
2022-04-23 16:24:25 +0300

    TICKET-000: Update storybook conf

--- apps/frontend/.storybook/main.js
+++ apps/frontend/.storybook/main.js
@@ -1,32 +1,31 @@
-const svgr = require("vite-plugin-svgr");
+const svgr = require('vite-plugin-svgr')
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   reactOptions: {
     fastRefresh: true,
   },
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
     {
-      name: "@storybook/addon-postcss",
+      name: '@storybook/addon-postcss',
       options: {
         postcssLoaderOptions: {
-          implementation: require("postcss"),
+          implementation: require('postcss'),
         },
       },
     },
-    // Commented out due to a bug https://github.com/eirslett/storybook-builder-vite/issues/242
-    // "@storybook/addon-interactions"
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "storybook-builder-vite",
+    builder: '@storybook/builder-vite',
   },
   // Storybook Vite builder does NOT read vite.config.ts, instead
   // it offers this hook where we can hack the config together
   async viteFinal(config, { configType }) {
-    config.plugins = [svgr(), ...config.plugins];
-    return config;
+    config.plugins = [svgr(), ...config.plugins]
+    return config
   },
-};
+}
